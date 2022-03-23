83a80b0aabb3032c9d1ac0b071a82b3757602913
Lucas Ramos <lucasnramos@gmail.com>
2022-03-23 11:08:11 -0300

    Merge with origin master

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,14 +2,14 @@ local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
 
 -- testing keymaps
-vim.api.nvim_set_keymap('n', '<leader>tt', ':Telescope builtin<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-p>', ':Telescope find_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader><C-f>', ':Telescope live_grep<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope git_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fds', ':Telescope lsp_document_symbols<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fws', ':Telescope lsp_workspace_symbols<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<M-p>', ':Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tp', ':Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tb', ':Telescope builtin<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tl', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>th', ':Telescope help_tags<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tff', ':Telescope git_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tfs', ':Telescope lsp_document_symbols<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tfws', ':Telescope lsp_workspace_symbols<CR>', { noremap = true, silent = true })
 
 require("telescope").setup {
   defaults = {

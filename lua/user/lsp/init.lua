7e5d43958a4a3ce6870ad44ff508384c424e7354
Lucas Ramos <lucas.ramos@gft.com>
2023-01-12 11:38:37 -0300

    cleanup for kickstart

--- lua/user/lsp/init.lua
+++
@@ -1,9 +0,0 @@
--- This will hold all LSP configs
-local status_ok, lsp = pcall(require, "lsp-zero")
-if not status_ok then
-  return
-end
-
-lsp.preset('recommended')
-lsp.nvim_workspace()
-lsp.setup()

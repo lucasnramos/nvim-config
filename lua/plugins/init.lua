911bc071e4dbb2b3ee8f7a40748ee8a428f8b170
Lucas Ramos <lucasnramos@gmail.com>
2024-10-01 16:53:09 -0300

    add changes to telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,7 +17,7 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-live-grep-args.nvim" },
 		config = function()
 			require("telescope").setup({
 				defaults = {
@@ -33,7 +33,7 @@ return {
 				},
 			})
 
-			vim.keymap.set("n", "<C-p>", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
+			vim.keymap.set("n", "<C-p>", require("telescope.builtin").find_files, { desc = "Search Files" })
 			vim.keymap.set("n", "<leader>o", require("telescope.builtin").buffers, { desc = "[S]earch [B]uffers" })
 			vim.keymap.set(
 				"n",
@@ -50,7 +50,12 @@ return {
 				require("telescope.builtin").grep_string,
 				{ desc = "[S]earch current [W]ord" }
 			)
-			vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set(
+				"n",
+				"<leader>sg",
+				":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+				{ desc = "[S]earch by [G]rep" }
+			)
 			vim.keymap.set(
 				"n",
 				"<leader>sd",
@@ -58,6 +63,7 @@ return {
 				{ desc = "[S]earch [D]iagnostics" }
 			)
 			vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
+			vim.keymap.set("n", "<leader>sb", require("telescope.builtin").buffers, { desc = "[S]earch [B]uffers" })
 		end,
 	},
 	{
